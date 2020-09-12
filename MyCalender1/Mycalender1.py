aaeb7db14e73f2665175c6f21e68cfe2fff468dd
Saksham Lubana <Lubana.Saksham@gmail.com>
2020-09-12 11:51:19 -0700

    I added calender1

---
+++ MyCalender1/Mycalender1.py
@@ -0,0 +1,16 @@
+#My code prints out my calender for the first 2 days of my school.
+print(" Day       Time             Subject\n"
+      "--------------------------------------\n"
+      "Monday   9:10-10:15         P.E.\n"
+      "Monday   10:15-10:35        Break\n"
+      "Monday   10:35-11:40        Exploratory wheel\n"
+      "Monday   11:40-12:10        Lunch\n"
+      "Monday   12:10-1:15         Science\n"
+      "Monday   1:15-3:00          Independent work time\n"
+      "Tuesday  9:10-10:15         Math\n"
+      "Tuesday  10:15-10:35        Break\n"
+      "Tuesday  10:35-11:40        Language arts\n"
+      "Tuesday  11:40-12:10        Lunch\n"
+      "Tuesday  12:10-1:15         Social Studies\n"
+      "Tuesday  1:15-3:00          Independent work")
+

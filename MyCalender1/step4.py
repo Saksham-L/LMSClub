809ba78a32a1d8101018ba8aaf658aa771646bba
Saksham Lubana <Lubana.Saksham@gmail.com>
2020-10-17 13:42:11 -0700

    I did part 3

---
+++ MyCalender1/step4.py
@@ -0,0 +1,22 @@
+Mycalender1 = (" Day       Time             Subject\n"
+               "--------------------------------------\n"
+               "Monday   9:10-10:15         P.E.\n"
+               "Monday   10:15-10:35        Break\n"
+               "Monday   10:35-11:40        Exploratory wheel\n"
+               "Monday   11:40-12:10        Lunch\n"
+               "Monday   12:10-1:15         Science\n"
+               "Monday   1:15-3:00          Independent work time\n"
+               "Tuesday  9:10-10:15         Math\n"
+               "Tuesday  10:15-10:35        Break\n"
+               "Tuesday  10:35-11:40        Language arts\n"
+               "Tuesday  11:40-12:10        Lunch\n"
+               "Tuesday  12:10-1:15         Social Studies\n"
+               "Tuesday  1:15-3:00          Independent work")
+
+if "Monday" in Mycalender1:
+    print("There is a Monday in my calender")
+else:
+    print("There is not a Monday in my calender")
+#this will find the length of my cslender.
+x = len(Mycalender1)
+print ("This is the length of my calender -",x)
